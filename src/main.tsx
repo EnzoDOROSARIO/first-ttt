5c4e39c703ed613377bd5483703f016217323969
Enzo DO ROSARIO <contact@edorosario.fr>
2024-08-22 22:48:02 +0200

    final code

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ContextProvider } from "./ContextProvider.tsx";
+import { provideTeaAdapter, Tea } from "./teaContext.tsx";
 
 const FIVE_MINUTES = 1000 * 60 * 5;
 
@@ -14,10 +16,31 @@ const queryClient = new QueryClient({
   },
 });
 
+const realImpl = {
+  fetchTeas: async (): Promise<Tea[]> => {
+    const res = await fetch("http://localhost:3000/teas");
+    return (await res.json()).data;
+  },
+};
+
+const fakeImpl = {
+  fetchTeas: async (): Promise<Tea[]> => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return [
+      { name: "Green Tea", description: "Green", color: "Green" },
+      { name: "Black Tea", description: "Black", color: "Black" },
+      { name: "White Tea", description: "White", color: "White" },
+      { name: "Oolong Tea", description: "Oolong", color: "Oolong" },
+    ];
+  },
+};
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ContextProvider providers={[provideTeaAdapter(fakeImpl)]}>
+        <App />
+      </ContextProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
